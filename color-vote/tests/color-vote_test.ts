d3257b498646758ba4c67b743ae275669a518e62
Ezra Kewa <88113467+Melchizedek4ever@users.noreply.github.com>
2024-11-06 12:23:28 +0100

    Update color-vote_test.ts
    Added tests for get-nb-of-voters and vote functions

--- color-vote/tests/color-vote_test.ts
+++ color-vote/tests/color-vote_test.ts
@@ -3,24 +3,34 @@ import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarine
 import { assertEquals } from 'https://deno.land/std@0.170.0/testing/asserts.ts';
 
 Clarinet.test({
-    name: "Ensure that <...>",
-    async fn(chain: Chain, accounts: Map<string, Account>) {
-        let block = chain.mineBlock([
-            /*
-             * Add transactions with:
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
-
-        block = chain.mineBlock([
-            /*
-             * Add transactions with:
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
+    name: '`get-nb-of-voters` - returns the right number of voters',
+    fn(chain: Chain, accounts: Map<string, Account>) {
+     
+      const { address } = accounts.get('wallet_1')!
+  
+      const block = chain.mineBlock([
+        Tx.contractCall('color-vote', 'get-nb-of-voters', [], address),
+      ])
+  
+      block.receipts[0].result.expectUint(0)
     },
-});
+  })
+
+  Clarinet.test({
+    name: '`vote` - participant can vote only one time',
+    fn(chain: Chain, accounts: Map<string, Account>) {
+      const { address } = accounts.get('wallet_1')!
+  
+      const vote = [types.uint(5), types.uint(5), types.uint(5), types.uint(5)]
+  
+      const block = chain.mineBlock([
+        Tx.contractCall('color-vote', 'vote', vote, address),
+        Tx.contractCall('color-vote', 'vote', vote, address),
+      ])
+  
+      // check first receipt
+      block.receipts[0].result.expectOk().expectBool(true)
+      // check second receipt
+      block.receipts[1].result.expectErr().expectUint(403)
+    },
+  })
